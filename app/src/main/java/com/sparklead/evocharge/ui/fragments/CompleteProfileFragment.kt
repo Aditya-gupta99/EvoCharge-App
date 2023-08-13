24c503f3f223d6986067abfb28688700fb04b450
Aditya Gupta <ashishgupta9936ad@gmail.com>
2023-08-14 01:28:44 +0530

    Added signin and profile page

---
+++ app/src/main/java/com/sparklead/evocharge/ui/fragments/CompleteProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.sparklead.evocharge.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sparklead.evocharge.databinding.FragmentCompleteProfileBinding
+
+class CompleteProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentCompleteProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCompleteProfileBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/sparklead/evocharge/ui/fragments/SignInFragment.kt
@@ -0,0 +1,23 @@
+package com.sparklead.evocharge.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sparklead.evocharge.databinding.FragmentSignInBinding
+
+class SignInFragment : Fragment() {
+
+    private var _binding: FragmentSignInBinding? = null
+    private val binding: FragmentSignInBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
